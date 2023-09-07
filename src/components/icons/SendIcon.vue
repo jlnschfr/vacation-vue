41007ef9c5f1019e3bbda99bf21bae4eb8fa9642
Julian Schäfer <me@julian-schaefer.net>
2023-09-08 00:43:46 +0200

    add icons

---
+++ src/components/icons/SendIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M36.667 3.333 18.333 21.667M36.667 3.333 25 36.667l-6.667-15-15-6.667L36.667 3.333Z"
+    />
+  </svg>
+</template>
