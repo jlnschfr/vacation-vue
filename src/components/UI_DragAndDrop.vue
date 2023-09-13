ea2dc349db38fe7c64c56c27501351436217311b
Julian Schäfer <me@julian-schaefer.net>
2023-09-13 14:58:05 +0200

    finetune image composition and add loading animation

--- src/components/UI_DragAndDrop.vue
+++ src/components/UI_DragAndDrop.vue
@@ -2,6 +2,7 @@
 import FileIcon from './icons/FileIcon.vue'
 import UploadIcon from './icons/UploadIcon.vue'
 import CheckedIcon from './icons/CheckedIcon.vue'
+import SendIcon from './icons/SendIcon.vue'
 import { ref, type Ref } from 'vue'
 
 export interface Props {
@@ -30,12 +31,15 @@ function onChange(event: Event): void {
 }
 
 function setDropped(files: FileList): void {
-  state.value = 'dropped'
-
-  if (files.length) {
-    file.value = files[0]
-    emits('fileUpdated', file.value)
+  if (!files.length) return
+  if (!(files[0].type.includes('image/png') || files[0].type.includes('image/jpeg'))) {
+    state.value = 'default'
+    return
   }
+
+  state.value = 'dropped'
+  file.value = files[0]
+  emits('fileUpdated', file.value)
 }
 </script>
 
@@ -44,8 +48,7 @@ function setDropped(files: FileList): void {
     class="outer"
     :class="{
       isOver: state === 'over',
-      isDropped: state === 'dropped',
-      isProcessing: isProcessing
+      isDropped: state === 'dropped'
     }"
   >
     <div
@@ -68,6 +71,18 @@ function setDropped(files: FileList): void {
         <p for="file" class="u-text-bold">{{ file.name }} selected</p>
       </div>
     </div>
+
+    <Transition>
+      <div v-if="isProcessing" class="animation">
+        <SendIcon class="icon" />
+        <SendIcon class="icon" />
+        <SendIcon class="icon" />
+        <SendIcon class="icon" />
+        <SendIcon class="icon" />
+        <SendIcon class="icon" />
+        <SendIcon class="icon" />
+      </div>
+    </Transition>
   </div>
 </template>
 
@@ -77,16 +92,8 @@ function setDropped(files: FileList): void {
   block-size: 20rem;
   margin-block-start: 3rem;
   padding: 1rem;
+  position: relative;
   transition: all 150ms ease-in-out;
-}
-
-.outer.isProcessing,
-.outer.isProcessing .inner {
-  background-color: #00f !important;
-}
-
-.outer.isOver {
-  padding: 0;
 }
 
 .inner {
@@ -101,6 +108,9 @@ function setDropped(files: FileList): void {
   transition: all 150ms ease-in-out;
 }
 
+.outer.isOver {
+  padding: 0;
+}
 .outer.isOver .inner {
   background-color: var(--color-red-light);
   border: 0.25rem dashed var(--color-white);
@@ -134,5 +144,94 @@ input:focus + label {
 .icon {
   height: auto;
   width: 3rem;
+}
+
+.animation {
+  align-items: center;
+  background-color: var(--color-red-light);
+  block-size: 100%;
+  color: var(--color-white);
+  display: flex;
+  inline-size: 100%;
+  inset: 0;
+  justify-content: center;
+  overflow: hidden;
+  position: absolute;
+  transition: all 150ms ease-in-out;
+}
+
+.animation .icon {
+  left: -50%;
+  position: absolute;
+  top: 150%;
+}
+
+.animation .icon:nth-child(1) {
+  animation: animation 4500ms 0ms linear infinite;
+  width: 5rem;
+}
+
+.animation .icon:nth-child(2) {
+  animation: animation 3500ms 500ms linear infinite;
+  transform: translate3d(0, -150%, 0);
+  width: 8rem;
+}
+
+.animation .icon:nth-child(3) {
+  animation: animation 5000ms 1000ms linear infinite;
+  transform: translate3d(0, 0, 0);
+  width: 8rem;
+}
+
+.animation .icon:nth-child(4) {
+  animation: animation 5500ms 1500ms linear infinite;
+  transform: translate3d(0, -100%, 0);
+  width: 12rem;
+}
+
+.animation .icon:nth-child(5) {
+  animation: animation 4000ms 2000ms linear infinite;
+  transform: translate3d(0, -300%, 0);
+  width: 4rem;
+}
+
+.animation .icon:nth-child(6) {
+  animation: animation 3500ms 2500ms linear infinite;
+  transform: translate3d(0, -50%, 0);
+  width: 6rem;
+}
+
+.animation .icon:nth-child(7) {
+  animation: animation 5500ms 3000ms linear infinite;
+  transform: translate3d(0, 100%, 0);
+  width: 8rem;
+}
+
+.animation .icon:nth-child(8) {
+  animation: animation 4500ms 3500ms linear infinite;
+  transform: translate3d(0, -200%, 0);
+  width: 12rem;
+}
+
+@keyframes animation {
+  from {
+    left: -50%;
+    top: 150%;
+  }
+
+  to {
+    left: 150%;
+    top: -50%;
+  }
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 250ms ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
--- src/components/VacationForm.vue
+++ src/components/VacationForm.vue
@@ -2,7 +2,7 @@
 import Input from './UI_Input.vue'
 import DragAndDrop from './UI_DragAndDrop.vue'
 import Button from './UI_Button.vue'
-import { reactive, ref, type Ref } from 'vue'
+import { ref, type Ref } from 'vue'
 import { createApi } from 'unsplash-js'
 import imglyRemoveBackground from '@imgly/background-removal'
 import mergeImages from 'merge-images'
@@ -13,30 +13,112 @@ const mergedImageSrc: Ref<string> = ref('')
 const isProcessing: Ref<boolean> = ref(false)
 const isDone: Ref<boolean> = ref(false)
 const hasError: Ref<boolean> = ref(false)
+const orientation: Ref<Orientation | string> = ref('')
 
-const aspectRatio = reactive({
-  width: 0,
-  height: 0
-})
+type Orientation = 'landscape' | 'portrait' | 'squarish'
+
+interface ImageSource {
+  blob: Blob
+  url: string
+}
+
+interface Details {
+  width: number
+  height: number
+  orientation: Orientation
+}
 
 function onInput(input: string) {
   query.value = input
   hasError.value = false
 }
 
-function onFileUpdate(userFile: File) {
+async function onFileUpdate(userFile: File) {
   file.value = userFile
   hasError.value = false
+
+  const details = await getImageDetails(file.value)
+  orientation.value = details.orientation
 }
 
-function getButtonLabel() {
-  if (isProcessing.value) {
-    return 'You are traveling...'
-  } else if (isDone.value) {
-    return 'I want to do it again!'
-  } else {
-    return 'Start my adventure'
+function getImageDetails(source: File | Blob): Promise<Details> {
+  return new Promise<Details>((resolve) => {
+    const reader = new FileReader()
+    const img = new Image()
+
+    reader.addEventListener(
+      'load',
+      () => {
+        img.addEventListener('load', () => {
+          const ratio = img.width / img.height
+          let orientation: Orientation
+
+          if (ratio > 1) {
+            orientation = 'landscape'
+          } else if (ratio < 1) {
+            orientation = 'portrait'
+          } else {
+            orientation = 'squarish'
+          }
+
+          resolve({
+            orientation,
+            width: img.width,
+            height: img.height
+          })
+        })
+
+        if (typeof reader.result === 'string') img.src = reader.result
+      },
+      false
+    )
+
+    reader.readAsDataURL(source)
+  })
+}
+
+function getRandomImage(): Promise<ImageSource> {
+  const unsplash = createApi({ accessKey: 'H77B8WG-2ePtnxonLBTJAPs8gomrotELt1J-9fbvVOQ' })
+  return new Promise((resolve) => {
+    unsplash.photos
+      .getRandom({ query: query.value, orientation: orientation.value })
+      .then((result) => {
+        if (result.errors) {
+          console.log('error occurred: ', result.errors[0])
+        } else {
+          const imageUrl: string = result.response.urls.regular
+
+          fetch(imageUrl).then((response) =>
+            response.blob().then((blob) => {
+              const url = URL.createObjectURL(blob)
+              resolve({ url, blob })
+            })
+          )
+        }
+      })
+  })
+}
+
+function removeBackground(file: File): Promise<ImageSource> {
+  let config = {
+    publicPath: 'public',
+    model: 'medium'
   }
+  return new Promise((resolve) => {
+    imglyRemoveBackground(file, config).then((blob: Blob) => {
+      const url: string = URL.createObjectURL(blob)
+      resolve({ url, blob })
+    })
+  })
+}
+
+async function merge(background: ImageSource, foreground: ImageSource) {
+  const details = await getImageDetails(foreground.blob)
+
+  return await mergeImages([foreground.url, background.url], {
+    width: details.width,
+    height: details.height
+  })
 }
 
 async function onSubmit() {
@@ -50,50 +132,24 @@ async function onSubmit() {
   }
 
   isProcessing.value = true
-  const sources: string[] = await Promise.all([removeBackground(file.value), getRandomImage()])
-  mergedImageSrc.value = await mergeImages(sources.reverse())
+  const [imageBackground, imageForeground]: ImageSource[] = await Promise.all([
+    removeBackground(file.value),
+    getRandomImage()
+  ])
 
+  mergedImageSrc.value = await merge(imageBackground, imageForeground)
   isProcessing.value = false
   isDone.value = true
 }
 
-function removeBackground(file: File): Promise<string> {
-  let config = {
-    publicPath: 'public',
-    model: 'small'
+function getButtonLabel() {
+  if (isProcessing.value) {
+    return 'You are traveling...'
+  } else if (isDone.value) {
+    return 'I want to do it again!'
+  } else {
+    return 'Start my adventure'
   }
-  return new Promise((resolve) => {
-    imglyRemoveBackground(file, config).then((blob: Blob) => {
-      const url: string = URL.createObjectURL(blob)
-      resolve(url)
-    })
-  })
-}
-
-function getRandomImage(): Promise<string> {
-  const unsplash = createApi({ accessKey: 'H77B8WG-2ePtnxonLBTJAPs8gomrotELt1J-9fbvVOQ' })
-  return new Promise((resolve) => {
-    unsplash.photos.getRandom({ query: query.value }).then((result) => {
-      if (result.errors) {
-        console.log('error occurred: ', result.errors[0])
-      } else {
-        console.log(result.response)
-        const imageUrl: string = result.response.urls.regular
-        aspectRatio.width = result.response.width
-        aspectRatio.height = result.response.height
-
-        console.log(aspectRatio.width)
-        console.log(aspectRatio.height)
-
-        fetch(imageUrl).then((response) =>
-          response.blob().then((blob) => {
-            const randomPhotoUrl = URL.createObjectURL(blob)
-            resolve(randomPhotoUrl)
-          })
-        )
-      }
-    })
-  })
 }
 </script>
 
@@ -123,6 +179,7 @@ img {
   color: var(--color-red);
   margin-block-start: 0.5rem;
 }
+
 .error.hidden {
   display: none;
 }
