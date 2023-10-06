7b4e98a03ec3da898e9686aa190d3a1489f21b6a
Julian Schäfer <me@julian-schaefer.net>
2023-10-06 17:27:54 +0200

    netlify functions test

--- netlify/functions/random-image/random-image.js
+++ netlify/functions/random-image/random-image.js
@@ -1,8 +1,12 @@
+/* globals process */
+
 export const handler = async () => {
+  const value = process.env.UNSPLASH_URL
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello World!'
+      message: value
     })
   }
 }
